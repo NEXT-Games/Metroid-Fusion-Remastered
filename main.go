4510d34d47add867d2220ae29c9ec935082d690f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-05-21 12:01:53 -0400

    Update main.go

--- main.go
+++ main.go
@@ -25,9 +25,10 @@ func (*menuScene) Setup(u engo.Updater) {
 	// Setup Scene
 	world, _ := u.(*ecs.World)
 	world.AddSystem(&common.RenderSystem{})
+	world.AddSystem(&movingThingSystem{})
 	// Setup Samus
 	sammy := Samus{BasicEntity: ecs.NewBasic()}
-	sammy.SpaceComponent = common.SpaceComponent{
+	sammy.SpaceComponent := common.SpaceComponent{
 		Position: engo.Point{0, 0},
 		Width: 1024,
 		Height: 576,
@@ -36,18 +37,32 @@ func (*menuScene) Setup(u engo.Updater) {
 	if err !=  nil {
 		log.Println("[FATAL] Can't load sprite for Samus! Error: " + err.Error())
 	}
-	sammy.RenderComponent = common.RenderComponent{
+	sammy.RenderComponent := common.RenderComponent{
 		Drawable: tex,
 		Scale: engo.Point{1, 1},
 	}
 	for _, system := range world.Systems(){
 		switch sys := system.(type){
 		case *common.RenderSystem:
-			sys.Add(&sammy.BasicEntity, &sammy.RenderComponent, &sammy.SpaceComponent)
+			sys.Add(&menuScene.sammy.BasicEntity, &menuScene.sammy.RenderComponent, &menuScene.sammy.SpaceComponent)
+		case *movingThingSystem:
+			sys.Add(&menuScene.sammy.SpaceComponent)
 		}
 	}
-	sammy.SpaceComponent.setPosition(engo.Point{2, 2})
 }
+
+type movingThingSystem struct {}
+
+func (*movingThingSystem) Type() string { return "movingThingSystem" }
+
+func (self *movingThingSystem) Update(dt float32) {
+	self.added.SpaceComponent.Position = engo.Point{100, 100}
+}
+
+func (self *movingThingSystem) Add(added common.SpaceComponent) {
+	self.added := added
+}
+
 
 func main() {
 	opts := engo.RunOptions{
