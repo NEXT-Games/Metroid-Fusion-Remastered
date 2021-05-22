be8be57b56ff02de96446e2bf547a6cff41f5ccc
zurgeg <zurgeg@gmail.com>
2021-05-22 18:34:45 -0400

    Adjust jump speed

--- main.go
+++ main.go
@@ -74,13 +74,13 @@ func (self *movingThingSystem) Update(dt float32) {
 		self.spaceComponent.Position.X += 3
 	}
 	if engo.Input.Button("Jump").Down() && !self.isJumping && self.totalJump <= 100 {
-		self.spaceComponent.Position.Y -= 10
-		self.totalJump += 10
+		self.spaceComponent.Position.Y -= 50
+		self.totalJump += 50
 		self.isJumping = true
 	}
 	if self.isJumping && self.totalJump <= 100 {
-		self.spaceComponent.Position.Y -= 10
-		self.totalJump += 10
+		self.spaceComponent.Position.Y -= 50
+		self.totalJump += 50
 	}
 	if self.isJumping && self.totalJump >= 100 {
 		self.totalJump = 0
