0b080ba2becec55fa7d266452749aebb2e917c06
zurgeg <zurgeg@gmail.com>
2021-05-22 18:36:51 -0400

    Still too fast

--- main.go
+++ main.go
@@ -74,13 +74,13 @@ func (self *movingThingSystem) Update(dt float32) {
 		self.spaceComponent.Position.X += 3
 	}
 	if engo.Input.Button("Jump").Down() && !self.isJumping && self.totalJump <= 100 {
-		self.spaceComponent.Position.Y -= 15
-		self.totalJump += 15
+		self.spaceComponent.Position.Y -= 10
+		self.totalJump += 10
 		self.isJumping = true
 	}
 	if self.isJumping && self.totalJump <= 100 {
-		self.spaceComponent.Position.Y -= 15
-		self.totalJump += 15
+		self.spaceComponent.Position.Y -= 10
+		self.totalJump += 10
 	}
 	if self.isJumping && self.totalJump >= 100 {
 		self.totalJump = 0
