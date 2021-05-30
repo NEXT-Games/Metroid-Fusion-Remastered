3b9c4097172cf291372185193086cce4e5b296ab
zurgeg <zurgeg@gmail.com>
2021-05-30 09:01:13 -0400

    Fix some warnings

--- systems.go
+++ systems.go
@@ -14,35 +14,35 @@ type movementSystem struct {
 
 func (*movementSystem) Type() string { return "movementSystem" }
 
-func (self *movementSystem) Update(dt float32) {
-	// self.spaceComponent.Position = engo.Point{100, 100}
+func (movementSystem *movementSystem) Update(dt float32) {
+	// movementSystem.spaceComponent.Position = engo.Point{100, 100}
 	// A friendly reminder that **we do NOT do a little trolling**
 	if engo.Input.Button("MoveLeft").Down() {
-		self.spaceComponent.Position.X -= 3
+		movementSystem.spaceComponent.Position.X -= 3
 	}
 	if engo.Input.Button("MoveRight").Down() {
-		self.spaceComponent.Position.X += 3
+		movementSystem.spaceComponent.Position.X += 3
 	}
-	if engo.Input.Button("Jump").Down() && !self.isJumping && self.totalJump <= 100 {
-		self.spaceComponent.Position.Y -= 20
-		self.totalJump += 20
-		self.isJumping = true
+	if engo.Input.Button("Jump").Down() && !movementSystem.isJumping && movementSystem.totalJump <= 100 {
+		movementSystem.spaceComponent.Position.Y -= 20
+		movementSystem.totalJump += 20
+		movementSystem.isJumping = true
 	}
-	if self.isJumping && self.totalJump <= 100 && self.totalJump >= -1 {
-		self.spaceComponent.Position.Y -= 20
-		self.totalJump += 20
+	if movementSystem.isJumping && movementSystem.totalJump <= 100 && movementSystem.totalJump >= -1 {
+		movementSystem.spaceComponent.Position.Y -= 20
+		movementSystem.totalJump += 20
 	}
-	if self.isJumping && self.totalJump >= 100 {
-		self.isJumping = false
+	if movementSystem.isJumping && movementSystem.totalJump >= 100 {
+		movementSystem.isJumping = false
 	}
 }
 
-func (self *movementSystem) Add(basicEntity *ecs.BasicEntity, renderComponent *common.RenderComponent, spaceComponent *common.SpaceComponent) {
-	self.spaceComponent = spaceComponent
-	self.isJumping = false
-	self.totalJump = 0
+func (movementSystem *movementSystem) Add(basicEntity *ecs.BasicEntity, renderComponent *common.RenderComponent, spaceComponent *common.SpaceComponent) {
+	movementSystem.spaceComponent = spaceComponent
+	movementSystem.isJumping = false
+	movementSystem.totalJump = 0
 }
 
-func (self *movementSystem) Remove(added ecs.BasicEntity) {
+func (movementSystem *movementSystem) Remove(added ecs.BasicEntity) {
 	// nop
 }
