a34e634b65cbac9f3ed8fb478f3869cc27f8dc49
zurgeg <zurgeg@gmail.com>
2021-05-30 09:31:11 -0400

    Finally collision works

---
+++ handlers.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/Noofbiz/engoBox2dSystem"
+)
+
+type entityType struct {
+	ecs.BasicEntity
+	*engoBox2dSystem.Box2dComponent
+}
+
+type entityHolder struct {
+	entities []*entityType
+}
+
+func (holder *entityHolder) Add(e *entityType) {
+	holder.entities = append(holder.entities, e)
+}
+
+func addListeners(s *entityHolder) {
+	engo.Mailbox.Listen("CollisionStartMessage", func(message engo.Message) {
+		c, isCollision := message.(engoBox2dSystem.CollisionStartMessage)
+		if isCollision {
+			a := c.Contact.GetFixtureA().GetBody().GetUserData()
+			b := c.Contact.GetFixtureB().GetBody().GetUserData()
+			if c.Contact.IsTouching() {
+				for i1, e1 := range s.entities {
+					if e1.BasicEntity.ID() == a || e1.BasicEntity.ID() == b {
+						for i2, e2 := range s.entities {
+							if i1 == i2 {
+								continue
+							}
+							if e2.BasicEntity.ID() == a || e2.BasicEntity.ID() == b {
+								continue // nop
+							}
+						}
+					}
+				}
+			}
+		}
+	})
+}
