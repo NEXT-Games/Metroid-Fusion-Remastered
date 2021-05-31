2ac6b1a8ff8aa89c1eeaa8207c4411c33d208c8f
zurgeg <zurgeg@gmail.com>
2021-05-31 12:39:12 -0400

    What about this?

--- systems.go
+++ systems.go
@@ -23,7 +23,7 @@ func (movementSystem *movementSystem) Update(dt float32) {
 	if engo.Input.Button("MoveRight").Down() {
 		movementSystem.spaceComponent.Position.X += 3
 	}
-	if engo.Input.Button("Jump").Down() && movementSystem.samus.totalJump <= 100 {
+	if engo.Input.Button("Jump").Down() && movementSystem.samus.totalJump > 100 {
 		movementSystem.samus.Body.ApplyLinearImpulseToCenter(box2d.B2Vec2{X: 0, Y: -1000}, true)
 		movementSystem.samus.totalJump += 10
 		movementSystem.samus.isJumping = true
