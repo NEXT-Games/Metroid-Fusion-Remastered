86015933520e4d1476ee8587ce06ead553167383
zurgeg <zurgeg@gmail.com>
2021-05-31 12:21:35 -0400

    log more stuff

--- handlers.go
+++ handlers.go
@@ -15,15 +15,19 @@ func addListeners(s *entityHolder) {
 			b := c.Contact.GetFixtureB().GetBody().GetUserData()
 			if c.Contact.IsTouching() {
 				for i1, e1 := range s.entities {
+					log.Printf("i1: %d", i1)
 					if e1.BasicEntity.ID() == a || e1.BasicEntity.ID() == b {
+						log.Println("e1 obtained")
 						for i2, e2 := range s.entities {
 							if i1 == i2 {
+								log.Println("i1 == i2")
 								continue
 							}
+							log.Printf("i2: %d", i1)
 							if e2.BasicEntity.ID() == a || e2.BasicEntity.ID() == b {
 								// This means samus has hit the floor and is no longer jumping.
 								// Stop the jump
-								log.Println("colstart")
+								log.Println("e1 and e2 obtained")
 								if e1.entity.spriteMeta == "samus" {
 									log.Println("e1")
 									e1.entity.totalJump = 0
