66253f915dd4d3d73390a1f77ce9c401e5de7118
zurgeg <zurgeg@gmail.com>
2021-06-01 11:22:31 -0400

    Adjust jumping properties

--- systems.go
+++ systems.go
@@ -26,8 +26,8 @@ func (movementSystem *movementSystem) Update(dt float32) {
 		movementSystem.spaceComponent.Position.X += 3
 	}
 	if engo.Input.Button("Jump").Down() && movementSystem.samus.totalJump < 100 {
-		movementSystem.samus.Body.ApplyLinearImpulseToCenter(box2d.B2Vec2{X: 0, Y: -5000}, true)
-		movementSystem.samus.totalJump += 1
+		movementSystem.samus.Body.ApplyLinearImpulseToCenter(box2d.B2Vec2{X: 0, Y: -1000}, true)
+		movementSystem.samus.totalJump += 5
 	}
 	if engo.Input.Button("Jump").Down() && !movementSystem.samus.canJump {
 		log.Printf("%d", movementSystem.samus.totalJump)
